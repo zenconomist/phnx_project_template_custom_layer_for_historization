b9feabcb4f1cb1433105cc39527648b6f6d07465
Kreisz Zsolt <kreiszzs@abylon.hu>
2024-12-25 21:08:12 +0100

    user data wrongly generated...

---
+++ phnx_project_template_history/lib/phnx_project_template_history_web/controllers/user_html.ex
@@ -0,0 +1,13 @@
+defmodule PhnxProjectTemplateHistoryWeb.UserHTML do
+  use PhnxProjectTemplateHistoryWeb, :html
+
+  embed_templates "user_html/*"
+
+  @doc """
+  Renders a user form.
+  """
+  attr :changeset, Ecto.Changeset, required: true
+  attr :action, :string, required: true
+
+  def user_form(assigns)
+end
---
+++ phnx_project_template_history/priv/templates/phx.gen.html/migration.exs
@@ -0,0 +1,25 @@
+create table(:<%= schema.table %>) do
+  <%= for {k, v} <- schema.types do %>add :<%= k %>, :<%= v %><%= schema.defaults[k] %>
+  <% end %>
+  timestamps_with_deleted_at()
+end
+
+
+create table(:<%= schema.table %>_history) do
+  add :entity_id, references(:<%= schema.table %>, on_delete: :nothing)
+  add :dat_from, :utc_datetime
+  add :dat_to, :utc_datetime
+  add :is_current, :boolean, default: true
+  <%= for {k, v} <- schema.types do %>add :<%= k %>, :<%= v %><%= schema.defaults[k] %>
+  <% end %>
+  timestamps_with_deleted_at()
+end
+
+create table(:<%= schema.table %>_field_log) do
+  add :entity_id, references(:<%= schema.table %>, on_delete: :nothing)
+  add :field, :string
+  add :old_value, :string
+  add :new_value, :string
+  add :action, :string
+  add :changed_at, :utc_datetime
+  timestamps()
