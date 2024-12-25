33b82e718bbc66b2cdb9f8eed51198e4001021a4
Kreisz Zsolt <kreiszzs@abylon.hu>
2024-12-26 00:18:19 +0100

    migrations fix

--- phnx_project_template_history/priv/repo/migrations/20241225211627_create_users.exs
+++ phnx_project_template_history/priv/repo/migrations/20241225211627_create_users.exs
@@ -1,40 +1,41 @@
 defmodule PhnxProjectTemplateHistory.Repo.Migrations.CreateUsers do
   use Ecto.Migration
   import Ecto.Migration
-  import PhnxProjectTemplateHistory.SchemaHelpers
+  require PhnxProjectTemplateHistory.SchemaHelpers
 
   def change do
     create table(:users) do
-        add :name, :string
-        add :email, :string
-        add :password, :string
-  
-        PhnxProjectTemplateHistory.SchemaHelpers.timestamps_with_deleted_at()
-      end
-  
-      create unique_index(:users, [:email])
-  
-    # create table(:users_history) do
-    #   add :entity_id, references(:users, on_delete: :nothing)
-    #   add :dat_from, :utc_datetime
-    #   add :dat_to, :utc_datetime
-    #   add :is_current, :boolean, default: true
-    #   add :name, :string
-    #   add :password, :string
-    #   add :email, :string
-      
-    #   PhnxProjectTemplateHistory.SchemaHelpers.timestamps_with_deleted_at()
-    # end
+      add :name, :string
+      add :email, :string
+      add :password, :string
 
-    # create table(:users_field_log) do
-    #   add :entity_id, references(:users, on_delete: :nothing)
-    #   add :field, :string
-    #   add :old_value, :string
-    #   add :new_value, :string
-    #   add :action, :string
-    #   add :changed_at, :utc_datetime
-    #   PhnxProjectTemplateHistory.SchemaHelpers.timestamps_with_deleted_at()
-    # end
+      timestamps()
+      add :deleted_at, :utc_datetime
+    end
+
+    create unique_index(:users, [:email])
+
+    create table(:users_history) do
+      add :entity_id, references(:users, on_delete: :nothing)
+      add :dat_from, :utc_datetime
+      add :dat_to, :utc_datetime
+      add :is_current, :boolean, default: true
+      add :name, :string
+      add :password, :string
+      add :email, :string
+
+      timestamps()
+    end
+
+    create table(:users_field_log) do
+      add :entity_id, references(:users, on_delete: :nothing)
+      add :field, :string
+      add :old_value, :string
+      add :new_value, :string
+      add :action, :string
+      add :changed_at, :utc_datetime
+
+      timestamps()
+    end
   end
-
 end
--- phnx_project_template_history/priv/templates/phx.gen.schema/migration.exs
+++ phnx_project_template_history/priv/templates/phx.gen.schema/migration.exs
@@ -1,6 +1,5 @@
 defmodule <%= inspect schema.repo %>.Migrations.Create<%= Macro.camelize(schema.table) %> do
   use <%= inspect schema.migration_module %>
-  import PhnxProjectTemplateHistory.SchemaHelpers
 
   def change do
     create table(:<%= schema.table %><%= if schema.binary_id do %>, primary_key: false<% end %><%= if schema.prefix do %>, prefix: :<%= schema.prefix %><% end %>) do
@@ -8,7 +7,8 @@ defmodule <%= inspect schema.repo %>.Migrations.Create<%= Macro.camelize(schema.
   <% end %><%= for {k, v} <- schema.attrs do %>      add <%= inspect k %>, <%= inspect Mix.Phoenix.Schema.type_for_migration(v) %><%= schema.migration_defaults[k] %>
   <% end %><%= for {_, i, _, s} <- schema.assocs do %>      add <%= inspect(i) %>, references(<%= inspect(s) %>, on_delete: :nothing<%= if schema.binary_id do %>, type: :binary_id<% end %>)
   <% end %>
-        PhnxProjectTemplateHistory.SchemaHelpers.timestamps_with_deleted_at()
+      timestamps(<%= if schema.timestamp_type != :naive_datetime, do: "type: #{inspect schema.timestamp_type}" %>)
+      add :deleted_at, :utc_datetime
       end
   <%= if Enum.any?(schema.indexes) do %><%= for index <- schema.indexes do %>
       <%= index %><% end %>
@@ -24,7 +24,8 @@ defmodule <%= inspect schema.repo %>.Migrations.Create<%= Macro.camelize(schema.
       add :is_current, :boolean, default: true
       <%= for {k, v} <- schema.types do %>add :<%= k %>, :<%= v %><%= schema.defaults[k] %>
       <% end %>
-      PhnxProjectTemplateHistory.SchemaHelpers.timestamps_with_deleted_at()
+      timestamps(<%= if schema.timestamp_type != :naive_datetime, do: "type: #{inspect schema.timestamp_type}" %>)
+  
     end
   end
 
@@ -37,7 +38,7 @@ defmodule <%= inspect schema.repo %>.Migrations.Create<%= Macro.camelize(schema.
       add :new_value, :string
       add :action, :string
       add :changed_at, :utc_datetime
-      PhnxProjectTemplateHistory.SchemaHelpers.timestamps_with_deleted_at()
+      timestamps(<%= if schema.timestamp_type != :naive_datetime, do: "type: #{inspect schema.timestamp_type}" %>)
     end
   end
 
