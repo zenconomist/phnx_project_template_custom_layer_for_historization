1885468412dcc0de719c15d4f0c6e913de593181
Kreisz Zsolt <kreiszzs@abylon.hu>
2025-01-02 22:17:51 +0100

    generator bash command

--- mix_generator_commands.sh
+++ mix_generator_commands.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 cd phnx_project_template_history
 
+## Users
 mix gen_field_logs Accounts User users name:string email:string:unique username:string:unique phone:string password:string
 sleep 1
 mix gen_scd2 Accounts User users name:string email:string username:string phone:string password:string
