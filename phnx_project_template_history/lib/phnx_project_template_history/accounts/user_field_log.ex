c19ac20b405d95325ad8f5c8e4a88ea251e507a3
Kreisz Zsolt <kreiszzs@abylon.hu>
2025-01-02 22:09:59 +0100

    no common generator

---
+++ phnx_project_template_history/lib/phnx_project_template_history/accounts/user_field_log.ex
@@ -0,0 +1,24 @@
+defmodule PhnxProjectTemplateHistory.Accounts.UserFieldLog do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users_field_log" do
+    field :action, :string
+    field :new_value, :string
+    field :row_id, :integer
+    field :field_name, :string
+    field :old_value, :string
+    field :time_of_change, :utc_datetime
+    field :changed_by, :string
+    field :deleted_at, :utc_datetime, default: nil
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(user_field_log, attrs) do
+    user_field_log
+    |> cast(attrs, [:row_id, :action, :field_name, :old_value, :new_value, :time_of_change, :changed_by, :deleted_at])
+    |> validate_required([:row_id, :action, :field_name, :old_value, :new_value, :time_of_change, :changed_by])
+  end
+end
