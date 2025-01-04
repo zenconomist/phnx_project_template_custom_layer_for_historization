c98cdc54d968bc048fdead1570ef3d984428cd1a
Kreisz Zsolt <kreiszzs@abylon.hu>
2025-01-04 17:28:45 +0100

    generator mod

--- mix_generator_commands.sh
+++ mix_generator_commands.sh
@@ -3,8 +3,12 @@
 cd phnx_project_template_history
 
 ## Users
-mix gen_field_logs Accounts User users name:string email:string:unique username:string:unique phone:string password:string
+## fields are not necessary here
+mix gen_field_logs Accounts User users
 sleep 1
+
+## don't include indexes and constraints here
 mix gen_scd2 Accounts User users name:string email:string username:string phone:string password:string
+
 sleep 1
 mix phx.gen.html Accounts User users name:string email:string:unique username:string:unique phone:string password:string
